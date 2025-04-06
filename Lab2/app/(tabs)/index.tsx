54e7d4ff191349fa371a8b4f181fe11770c4e768
missShevel <o.v.shevel@gmail.com>
2025-04-06 20:16:04 +0300

    add community screen

--- Lab2/app/(tabs)/index.tsx
+++ Lab2/app/(tabs)/index.tsx
@@ -1,16 +1,16 @@
-import BannerCarousel from '@/components/BannerCarousel';
-import CategoryTile from '@/components/CategoryTile';
-import GameItem from '@/components/GameItem';
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import styled from 'styled-components/native';
-import { Ionicons } from '@expo/vector-icons';
-import { HeaderApp } from '@/components/Header';
-import { games } from '@/data/games';
+import BannerCarousel from "@/components/BannerCarousel";
+import CategoryTile from "@/components/CategoryTile";
+import GameItem from "@/components/GameItem";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
+import { HeaderApp } from "@/components/Header";
+import { games } from "@/data/games";
+import { Container } from "@/components/Container";
+import CategoryTiles from "@/components/CategoryTile";
 
-
-const categories = ['Top Sellers', 'Free to play', 'Early Access'];
-
+const categories = ["Top Sellers", "Free to play", "Early Access"];
 
 export default function Tab() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
@@ -21,37 +21,22 @@ export default function Tab() {
 
   return (
     <Container>
-      <View style={{padding: 16}}>
-      <HeaderApp headerTitle='Store' showIcon/>
-      <BannerCarousel />
+      
+        <HeaderApp headerTitle="Store" showIcon />
+        <BannerCarousel />
 
-      <CategoryScroll horizontal showsHorizontalScrollIndicator={false}>
-        {categories.map((cat) => (
-          <CategoryTile
-            key={cat}
-            label={cat}
-            selected={selectedCategory === cat}
-            onPress={() => setSelectedCategory(cat)}
-          />
-        ))}
-      </CategoryScroll>
+        <CategoryTiles
+          categories={categories}
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
-      <FlatList
-        data={filteredGames}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={({ item }) => <GameItem {...item} />}
-        contentContainerStyle={{ paddingBottom: 100 }}
-      />
-      </View>
+        <FlatList
+          data={filteredGames}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({ item }) => <GameItem {...item} />}
+          contentContainerStyle={{ paddingBottom: 100 }}
+        />
     </Container>
   );
 }
-
-const Container = styled.SafeAreaView`
-  flex: 1;
-  background-color: #1b1f2a;
-`;
-
-const CategoryScroll = styled.ScrollView`
-  margin-vertical: 16px;
-`;
--- Lab2/components/CategoryTile.tsx
+++ Lab2/components/CategoryTile.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React from "react";
+import styled from "styled-components/native";
+
+const CategoryScroll = styled.ScrollView`
+  margin-vertical: 16px;
+`;
+
+const TileButton = styled.TouchableOpacity<{ $selected: boolean }>`
+  padding: 8px 16px;
+  background-color: ${({ $selected }: { $selected: boolean }) =>
+    $selected ? "#31BCFC" : "#2c2f38"};
+  border-radius: 8px;
+  margin-right: 10px;
+  height: 38px;
+  width: auto;
+`;
+
+const TileText = styled.Text<{ $selected: boolean }>`
+  color: ${({ $selected }: { $selected: boolean }) =>
+    $selected ? "#fff" : "#bbb"};
+`;
 
 interface Props {
   label: string;
@@ -13,17 +32,27 @@ const CategoryTile = ({ label, selected, onPress }: Props) => (
   </TileButton>
 );
 
-export default CategoryTile;
+const CategoryTiles = ({
+  categories,
+  selectedCategory,
+  setSelectedCategory,
+}: {
+  categories: string[];
+  selectedCategory: string;
+  setSelectedCategory: (cat: string) => void;
+}) => {
+  return (
+    <CategoryScroll horizontal showsHorizontalScrollIndicator={false}>
+      {categories.map((cat) => (
+        <CategoryTile
+          key={cat}
+          label={cat}
+          selected={selectedCategory === cat}
+          onPress={() => setSelectedCategory(cat)}
+        />
+      ))}
+    </CategoryScroll>
+  );
+};
 
-const TileButton = styled.TouchableOpacity<{ $selected: boolean }>`
-  padding: 8px 16px;
-  background-color: ${({ $selected }: {$selected: boolean}) => ($selected ? '#31BCFC' : '#2c2f38')};
-  border-radius: 8px;
-  margin-right: 10px;
-  height: 38px;
-  width: auto;
-`;
-
-const TileText = styled.Text<{ $selected: boolean }>`
-  color: ${({ $selected }:{$selected: boolean}) => ($selected ? '#fff' : '#bbb')};
-`;
+export default CategoryTiles;
